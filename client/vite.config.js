83e74b69d9f2d7ae3083f6a89c7d0b2d30e18797
Mohammad Hani Dakkak <mhdakkak@MHDMAC.local>
2025-09-12 08:58:36 -0400

    5th fixing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  // existing config options...
+  plugins: [react()],
+  base: "/", // make sure this is "/"
   preview: {
     host: "0.0.0.0",
     port: process.env.PORT || 10000,
-    allowedHosts: [
-      "portfolio-49nt.onrender.com"
-    ]
+    allowedHosts: ["*"]
   }
 })
-
